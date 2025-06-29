63370f35c0b403ae9010171ce8c64df333835197
UIcodes89 <manojkshukla87@gmail.com>
2025-06-29 21:00:25 +0100

    add catalog and text file for test

---
+++ python-app/template/src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes',
+        'message1git status':'you are doing grat!! Manoj Shukla!',
+        'deployed_on':'kubernates',
+        'env': ${{values.app_env}},
+         'app': ${{values.app_name}}
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    # Do an actual check here
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+    # Start the server only once and after all routes are defgitined
+    app.run(host="0.0.0.0", port=8080)
